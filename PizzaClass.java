c500551de1a32a91c1bc1bfd4be502d50d0f45dc
Serzhik-a <81388670+Serzhik-a@users.noreply.github.com>
2021-03-29 13:00:55 +0300

    Add 2 files of extra functional

---
+++ PizzaClass.java
@@ -0,0 +1,22 @@
+package ru.home;
+
+public class PizzaClass {
+
+    public static final int maxSizePizza = 52;    //Максимальный размер пиццы
+    private int sizePizza;                   //Размер заказанной пицы
+    private String addInPizza;              //Пожелания по добавкам
+
+    public PizzaClass(int sizePizza, String addInPizza) {
+        this.sizePizza = sizePizza;
+        this.addInPizza = addInPizza;
+    }
+
+
+    public int getSizePizza() {
+        return sizePizza;
+    }
+
+    public String getAddInPizza() {
+        return addInPizza;
+    }
+}
